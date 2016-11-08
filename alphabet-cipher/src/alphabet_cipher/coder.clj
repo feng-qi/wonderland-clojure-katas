691bc0a6c015a89d19968f2c62ece14eaee62644
feng qi <feng qi>
2016-11-08 23:23:02 +0800

    alphabet-cipher: Add encode implementation

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -1,7 +1,42 @@
 (ns alphabet-cipher.coder)
 
+(def map-table
+  ["abcdefghijklmnopqrstuvwxyz"
+   "bcdefghijklmnopqrstuvwxyza"
+   "cdefghijklmnopqrstuvwxyzab"
+   "defghijklmnopqrstuvwxyzabc"
+   "efghijklmnopqrstuvwxyzabcd"
+   "fghijklmnopqrstuvwxyzabcde"
+   "ghijklmnopqrstuvwxyzabcdef"
+   "hijklmnopqrstuvwxyzabcdefg"
+   "ijklmnopqrstuvwxyzabcdefgh"
+   "jklmnopqrstuvwxyzabcdefghi"
+   "klmnopqrstuvwxyzabcdefghij"
+   "lmnopqrstuvwxyzabcdefghijk"
+   "mnopqrstuvwxyzabcdefghijkl"
+   "nopqrstuvwxyzabcdefghijklm"
+   "opqrstuvwxyzabcdefghijklmn"
+   "pqrstuvwxyzabcdefghijklmno"
+   "qrstuvwxyzabcdefghijklmnop"
+   "rstuvwxyzabcdefghijklmnopq"
+   "stuvwxyzabcdefghijklmnopqr"
+   "tuvwxyzabcdefghijklmnopqrs"
+   "uvwxyzabcdefghijklmnopqrst"
+   "vwxyzabcdefghijklmnopqrstu"
+   "wxyzabcdefghijklmnopqrstuv"
+   "xyzabcdefghijklmnopqrstuvw"
+   "yzabcdefghijklmnopqrstuvwx"
+   "zabcdefghijklmnopqrstuvwxy"])
+(defn extend-keyword [keyword msg]
+  (apply str (take (count msg) (apply str (repeat (count msg) keyword)))))
+
+(defn getXY [x y]
+  (nth (nth map-table x) y))
+
 (defn encode [keyword message]
-  "encodeme")
+  (apply str
+         (map #(apply getXY (list (- (int (first %)) (int \a)) (- (int (second %)) (int \a))))
+              (partition 2 (interleave (extend-keyword keyword message) message)))))
 
 (defn decode [keyword message]
   "decodeme")
