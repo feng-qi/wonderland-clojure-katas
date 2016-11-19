fe632f5b4c01a0a32d881c11248ceb4f338e28d2
fengqi <feng.qi@msn.com>
2016-11-19 23:06:34 +0800

    alphabet-cipher: improvement
    Remove the reliance of map-table, the table is not necessary

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -35,12 +35,17 @@
   (nth (nth map-table (mod x 26)) (mod y 26)))
 
 (defn get-repeat [in]
-  (loop [shifted (rest in)
-         n 1]
-    ;; (if (and (not= shifted '()) (not-every? true? (map = in shifted)))
-    (if (not-every? true? (map = in shifted))
-      (recur (rest shifted) (inc n))
+  (loop [n 1]
+    (if (not-every? true? (map = in (drop n in)))
+      (recur (inc n))
       (take n in))))
+
+;; (defn get-repeat [in]
+;;   (loop [shifted (rest in)
+;;          n 1]
+;;     (if (not-every? true? (map = in shifted))
+;;       (recur (rest shifted) (inc n))
+;;       (take n in))))
 
 (defn encode [keyword message]
   (apply str
@@ -60,4 +65,36 @@
    get-repeat
    (map #(+ % (int \a)))
    (map char)
+   (apply str)))
+
+(defn char-to-int [c]
+  (- (int c) (int \a)))
+
+(defn int-to-char [i]
+  (if (and (<= i (char-to-int \z))
+           (>= i (char-to-int \a)))
+    (char (+ i (int \a)))
+    (char (+ (mod i 26) (int \a)))))
+
+;; (char (+ -1 (mod i (char-to-int \z)) (int \a))))) ; wrong, should mode 26
+
+(defn encode [keyword message]
+  (->>
+   (extend-keyword keyword message)
+   (map #(+ (char-to-int %1) (char-to-int %2)) message)
+   (map int-to-char)
+   (apply str)))
+
+(defn decode [keyword message]
+  (->>
+   (extend-keyword keyword message)
+   (map #(- (char-to-int %1) (char-to-int %2)) message)
+   (map int-to-char)
+   (apply str)))
+
+(defn decipher [cipher message]
+  (->>
+   (map #(- (char-to-int %1) (char-to-int %2)) cipher message)
+   (map int-to-char)
+   get-repeat
    (apply str)))
